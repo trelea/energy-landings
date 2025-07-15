f789c709398711d9bc8d625f16418bdc1098d4c4
Roman <roman.ghidenco@isa.utm.md>
2025-07-16 00:09:25 +0300

    final

--- src/components/Landing/WhyUs.tsx
+++ src/components/Landing/WhyUs.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
+import { Shield, Zap, Users, Award } from 'lucide-react';
 
 export default function WhyUs() {
     const t = useTranslations('WhyUs');
@@ -10,9 +11,11 @@ export default function WhyUs() {
                 <span className='font-medium text-[32px] max-w-sm xl:text-center'>{t('title')}</span>
 
                 <div className='flex flex-col xl:flex-row gap-14'>
-                    <div className='flex flex-col gap-14'>
+                    <div className='flex flex-col gap-14 w-full'>
                         <div className='flex flex-col gap-8 items-start justify-center'>
-                            <div className='w-16 h-16 flex justify-center items-center text-white text-center text-2xl p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>01</div>
+                            <div className='w-16 h-16 flex justify-center items-center text-white p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>
+                                <Award size={32} />
+                            </div>
                             <div className='flex flex-col gap-4'>
                                 <span className='text-xl font-medium'>{t('points.1.title')}</span>
                                 <span className='text-[#666666]'>{t('points.1.description')}</span>
@@ -20,7 +23,9 @@ export default function WhyUs() {
                         </div>
 
                         <div className='flex flex-col gap-8 items-start justify-center'>
-                            <div className='w-16 h-16 flex justify-center items-center text-white text-center text-2xl p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>02</div>
+                            <div className='w-16 h-16 flex justify-center items-center text-white p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>
+                                <Users size={32} />
+                            </div>
                             <div className='flex flex-col gap-4'>
                                 <span className='text-xl font-medium'>{t('points.2.title')}</span>
                                 <span className='text-[#666666]'>{t('points.2.description')}</span>
@@ -30,9 +35,11 @@ export default function WhyUs() {
 
                     <Image src={'/whyus.svg'} alt='Why Us' width={500} height={500} className='hidden xl:block' />
 
-                    <div className='flex flex-col gap-14'>
+                    <div className='flex flex-col gap-14 w-full'>
                         <div className='flex flex-col gap-8 items-start xl:items-end justify-center'>
-                            <div className='w-16 h-16 flex justify-center items-center text-white text-center text-2xl p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>03</div>
+                            <div className='w-16 h-16 flex justify-center items-center text-white p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>
+                                <Zap size={32} />
+                            </div>
                             <div className='flex flex-col justify-center items-start xl:items-end gap-4'>
                                 <span className='text-xl font-medium'>{t('points.3.title')}</span>
                                 <span className='text-[#666666] text-start xl:text-end'>{t('points.3.description')}</span>
@@ -40,9 +47,11 @@ export default function WhyUs() {
                         </div>
 
                         <div className='flex flex-col gap-8 items-start xl:items-end justify-center'>
-                            <div className='w-16 h-16 flex justify-center items-center text-white text-center text-2xl p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>04</div>
+                            <div className='w-16 h-16 flex justify-center items-center text-white p-3 bg-gradient-to-b from-[#00BCD4] to-[#007E8F] rounded-full'>
+                                <Shield size={32} />
+                            </div>
                             <div className='flex flex-col justify-center items-start xl:items-end gap-4'>
-                                <span className='text-xl font-medium'>{t('points.4.title')}</span>
+                                <span className='text-xl font-medium lg:text-end'>{t('points.4.title')}</span>
                                 <span className='text-[#666666] text-start xl:text-end'>{t('points.4.description')}</span>
                             </div>
                         </div>
